6b61dbb0184f7a950688ffd073bbe5add6be54ff
Walkx01 <93958915+Walkx01@users.noreply.github.com>
2022-10-07 12:42:12 -0400

    React project code added

---
+++ build/precache-manifest.3f6ae0778c8a1d0ea324d3408e6f3456.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cb708935cc8a8bb8308",
+    "url": "/Walkx01/COMP3104/static/css/main.deb63344.chunk.css"
+  },
+  {
+    "revision": "6cb708935cc8a8bb8308",
+    "url": "/Walkx01/COMP3104/static/js/main.6cb70893.chunk.js"
+  },
+  {
+    "revision": "d85dfea96ea4d5179be6",
+    "url": "/Walkx01/COMP3104/static/js/runtime~main.d85dfea9.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Walkx01/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "3cf9a3763e97890c02fc08e0bb5e401b",
+    "url": "/Walkx01/COMP3104/index.html"
+  }
+];
